a67b88cd138eaeb1bfef191aadd2592ebc300298
Evanator05 <watsonevan2005@gmail.com>
2021-11-25 18:41:37 -0700

    hgtnbmyjv ,trfygvhbjnm6ytr5fgvhj

--- scr/index.py
+++ scr/index.py
@@ -32,4 +32,4 @@ def makeUser(username, password, firstName, lastName, bio):
     }
     return user
 
-pprint(makeUser("hi","abc","evan","watson","my name is evan and I love balls"))
+pprint(pullAPI())
