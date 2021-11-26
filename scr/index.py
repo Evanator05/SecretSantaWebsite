446175bd02e2e9e394f3adcb540e0b93baca3cd5
Evanator05 <watsonevan2005@gmail.com>
2021-11-25 23:18:15 -0700

    fadsf

--- scr/index.py
+++ scr/index.py
@@ -20,14 +20,14 @@ def encryptString(hash_string):#encrypt a string
     return hashlib.sha256(hash_string.encode()).hexdigest()
 
 def makeUser(username, password, firstName, lastName, bio):
-    user = {#make user JSON object
+    user = { #make user JSON object
         "name": {
             "username": username,
             "firstName": firstName,
             "lastName": lastName
         },
         "target":"Picked December First",
-        "password": encryptString(password),#password encrypted with sha256 making it uncrackable
+        "password": encryptString(password), #password encrypted with sha256 making it uncrackable
         "bio": bio
     }
     return user
