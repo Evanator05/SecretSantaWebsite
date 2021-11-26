1afacce38a87e3e490752b143f12020911a004e3
Evanator05 <watsonevan2005@gmail.com>
2021-11-25 17:43:07 -0700

    tyfgv

--- scr/index.py
+++ scr/index.py
@@ -1,4 +1,18 @@
-
+from random import choice as randchoice
+import requests
+from pprint import pprint
 
 def pullAPI():
-    pass
+    userData = requests.get("https://evanator05.github.io/SecretSantaServer/json/users.json")
+    data = userData.json()
+    pprint(data)
+
+def getPairs(people):
+    pairs = dict()
+    for person in people:
+        pair = randchoice(people)
+        while pair == person:
+            pair = randchoice(people)
+        pairs[person] = pair
+
+pullAPI()
