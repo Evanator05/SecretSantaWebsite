65ad6e73cd73f3a7a12f363c99e43abac999663b
Evanator05 <watsonevan2005@gmail.com>
2021-11-25 18:33:29 -0700

    thgrtgrhf

--- scr/index.py
+++ scr/index.py
@@ -3,18 +3,34 @@ from pprint import pprint
 import json, hashlib, requests
 
 def pullAPI():
-    data = requests.get("https://evanator05.github.io/SecretSantaServer/json/users.json").json()#get json from website
-    return data
-    pprint(len(data["users"]))
+    data = requests.get("https://evanator05.github.io/SecretSantaServer/json/users.json")
+    print(data)
+    if data.status_code == 200:
+        userData = data.json()
+    else:
+        print('Endpoint did not connect')
+        return None
+    return userData
 
-def getUserIndex(data, name):
+def getUserIndex(data, username):
     for i in range(len(data["users"])):
-        if data["users"][i]["name"]["username"] == name:
+        if data["users"][i]["name"]["username"] == username:
             return i
 
 def encryptString(hash_string):
-    sha_signature = hashlib.sha256(hash_string.encode()).hexdigest()
-    return sha_signature
+    return hashlib.sha256(hash_string.encode()).hexdigest()
 
+def makeUser(username, password, firstName, lastName, bio):
+    user = {
+        "name": {
+            "username": username,
+            "firstName": firstName,
+            "lastName": lastName
+        },
+        "target":"Picked December First",
+        "password": encryptString(password),
+        "bio": bio
+    }
+    return user
 
-with open('output.json', 'a') as out
+pullAPI()
