cdbf0486e7cf2dcba61688166762d8014246e87d
Evanator05 <watsonevan2005@gmail.com>
2021-11-29 16:56:22 -0700

    asdasd

--- scr/register.js
+++ scr/register.js
@@ -1,9 +1,7 @@
 var postURL = "http://192.168.1.70:25549/"
-const Http = new XMLHttpRequest();
-
+var pullURL = "http://192.168.1.70:25550/users.json"
 
 function makeUser(username, password, firstName, lastName, bio) {
-
       var user = { //make user JSON object
           "username": username,
           "firstName": firstName,
@@ -33,6 +31,17 @@ function makeUser(username, password, firstName, lastName, bio) {
       return user
 }
 
+
+function getUserIndex(data, username) {
+  for(let i = 0; i < data.users.length; i++) {
+    if (data.users[i].username == username) {
+      return i
+    } else {
+      return null
+    }
+  }
+}
+
 function registerAccount() {
   let username = document.getElementById("username").value
   let firstName = document.getElementById("firstName").value
@@ -40,9 +49,15 @@ function registerAccount() {
   let password = document.getElementById("password").value
   let confirmPassword = document.getElementById("confirmPassword").value
   let bio = document.getElementById("bio").value
-
   if (password == confirmPassword) {
     user = makeUser(username, password, firstName, lastName, bio)
     $.post(postURL, user);
+
+  } else {
+    returnError("Passwords do not match")
   }
+}
+
+function returnError(error) {
+  document.getElementById("error").innerHTML = error
 }
