6fa182e2923c66544cf8cfef9ae057a836f19e46
Evanator05 <watsonevan2005@gmail.com>
2021-11-29 19:39:06 -0700

    njhui

--- scr/register.js
+++ scr/register.js
@@ -11,21 +11,6 @@ function makeUser(username, password, firstName, lastName, bio) {
           "bio": bio
           }
 
-      function hash(string) {
-        const utf8 = new TextEncoder().encode(string);
-        return crypto.subtle.digest('SHA-256', utf8).then((hashBuffer) => {
-          const hashArray = Array.from(new Uint8Array(hashBuffer));
-          const hashHex = hashArray
-            .map((bytes) => bytes.toString(16).padStart(2, '0'))
-            .join('');
-          return hashHex;
-        });
-      }
-
-
-
-      hash(password).then(value => makeObject(value))
-      console.log(user)
       return user
 }
 
@@ -50,7 +35,6 @@ function registerAccount() {
   if (password == confirmPassword) {
     user = makeUser(username, password, firstName, lastName, bio)
     $.post(postURL, user);
-
   } else {
     returnError("Passwords do not match")
   }
